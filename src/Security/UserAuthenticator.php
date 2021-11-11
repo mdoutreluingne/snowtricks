36101aa371e7bd8657be5db97b76c9df1c98d74c
Maxime Doutreluingne <maxime.doutreluingne@sfr.fr>
2021-11-11 17:46:27 +0100

    feat: Add page for reset password && add translate message validator

--- src/Security/UserAuthenticator.php
+++ src/Security/UserAuthenticator.php
@@ -4,21 +4,22 @@ namespace App\Security;
 
 use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Security;
+use Symfony\Component\Security\Csrf\CsrfToken;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Http\Util\TargetPathTrait;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Csrf\CsrfTokenManagerInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+use Symfony\Component\Security\Guard\PasswordAuthenticatedInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
-use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Core\Exception\InvalidCsrfTokenException;
 use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
-use Symfony\Component\Security\Core\Security;
-use Symfony\Component\Security\Core\User\UserInterface;
-use Symfony\Component\Security\Core\User\UserProviderInterface;
-use Symfony\Component\Security\Csrf\CsrfToken;
-use Symfony\Component\Security\Csrf\CsrfTokenManagerInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Guard\Authenticator\AbstractFormLoginAuthenticator;
-use Symfony\Component\Security\Guard\PasswordAuthenticatedInterface;
-use Symfony\Component\Security\Http\Util\TargetPathTrait;
+use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
 
 class UserAuthenticator extends AbstractFormLoginAuthenticator implements PasswordAuthenticatedInterface
 {
@@ -72,6 +73,9 @@ class UserAuthenticator extends AbstractFormLoginAuthenticator implements Passwo
         if (!$user) {
             throw new UsernameNotFoundException('Username could not be found.');
         }
+        if ($user->isVerified() == false) {
+            throw new CustomUserMessageAuthenticationException("Votre compte n'est pas activé");
+        }
 
         return $user;
     }
@@ -95,7 +99,12 @@ class UserAuthenticator extends AbstractFormLoginAuthenticator implements Passwo
             return new RedirectResponse($targetPath);
         }
 
-        return new RedirectResponse($this->urlGenerator->generate('admin'));
+        //Redirect user with role ADMIN in panel admin
+        if ($token->getUser()->getRoles()[0] === "ROLE_ADMIN") {
+            return new RedirectResponse($this->urlGenerator->generate('admin'));
+        }
+
+        return new RedirectResponse($this->urlGenerator->generate('home'));
     }
 
     protected function getLoginUrl()
---
+++ src/Service/MailerService.php
@@ -0,0 +1,40 @@
+<?php 
+namespace App\Service;
+
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Component\Mailer\MailerInterface;
+
+class MailerService
+{
+    private $mailer;
+
+    public function __construct(MailerInterface $mailer)
+    {
+        $this->mailer = $mailer;
+    }
+
+    /**
+     * Send email
+     *
+     * @param string $from
+     * @param string $to
+     * @param string $subject
+     * @param string $pathtemplate
+     * @param array $parameters
+     * @return void
+     */
+    public function send(string $from, string $to, string $subject, string $pathtemplate, array $parameters)
+    {
+        $email = (new TemplatedEmail())
+            ->from($from)
+            ->to($to)
+            ->subject($subject)
+            ->htmlTemplate($pathtemplate)
+            ->context($parameters);
+
+        $this->mailer->send($email);
+    }
+}
+
+
+?>
